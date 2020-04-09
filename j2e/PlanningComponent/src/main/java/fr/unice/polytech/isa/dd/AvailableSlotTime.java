9b7fbf901789c823f3f6ce4261b13428772ebc9e
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-04-09 08:15:08 +0200

    #57 moving other j2e components and CliCustomerService

--- j2e/PlanningComponent/src/main/java/fr/unice/polytech/isa/dd/AvailableSlotTime.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.isa.dd;
-
-import javax.ejb.Local;
-
-@Local
-public interface AvailableSlotTime {
-
-    boolean valid_slot_time(String delivery_date, String hour_delivery) throws Exception;
-}
