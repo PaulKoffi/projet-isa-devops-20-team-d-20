7910ae4d9408c930f8b8f541153b8d6fe21fe795
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-04-09 04:17:13 +0200

    #53 fixing build of j2e server in docker and #57 moving entities to his own repo

--- docker/drone-delivery/resources/start-dd.sh
+++ docker/drone-delivery/resources/start-dd.sh
@@ -7,11 +7,6 @@ echo "bankHostName=$bank_host" >> ./WEB-INF/classes/bank.properties
 echo "bankPortNumber=$bank_port" >> ./WEB-INF/classes/bank.properties
 
 # step #2: update the webapp to load the right properties
-jar uvf ./webapps/BillingComponent.war ./WEB-INF/classes/bank.properties
-jar uvf ./webapps/BillingWebService.war ./WEB-INF/classes/bank.properties
-jar uvf ./webapps/CustomerRegisterComponent.war ./WEB-INF/classes/bank.properties
-jar uvf ./webapps/DeliveryComponent.war ./WEB-INF/classes/bank.properties
-jar uvf ./webapps/DeliveryWebService.war ./WEB-INF/classes/bank.properties
 jar uvf ./webapps/Web.war ./WEB-INF/classes/bank.properties
 
 # step #3: start the TomEE engine
