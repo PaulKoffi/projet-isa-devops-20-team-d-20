f624bfee8cd2ed9797cdab3f70e1445dd157642b
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-05-08 15:04:00 +0200

    writing build and run scripts

---
+++ client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## We are using here Clissandre's CLI because it's the most advanced CLI which cover all features of the demo
+
+## with java
+#cd client/Clissandre
+#cd target/classes && java Main
+
+## with docker
+docker attach djotiham/dd-client-clissandre
