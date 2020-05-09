ac0b538eb5747b45bbbc2d93f32ed3aeeb67f5fb
djotihamNABAGOU <djotiham.nabagou@etu.univ-cotedazur.fr>
2020-05-09 05:39:11 +0200

    updating jenkins url

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ## >>>>> Displaying authentification informations to jenkins server
-echo "URL de connexion au serveur Jenkins : " http://teamd-isadevops.francecentral.cloudapp.azure.com:8080
+echo "URL de connexion au serveur Jenkins : " http://jenkins-teamd.francecentral.cloudapp.azure.com:8080
 echo "Utilisateur : admin"
 echo "Mot de passe : teamd-isa2020"
